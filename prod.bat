f2a5aa9f8205654e904855eb4b86fd271d8bb098
Bryan Jarvis <pootwaddle@pootwaddle.com>
2021-06-04 22:31:00 -0500

    remove debug line

--- prod.bat
+++ prod.bat
@@ -1,35 +1,36 @@
+
 cd fileparse
-go get -u github.com/pootwaddle/mailenable/scrubbing@latest
+go get -u github.com/pootwaddle/mailenable/scrubbing@v0.2.1
 go mod tidy
 go build
 copy *.exe \\moe\d\bjtools\ /Y
 cd ..\greysplit
-go get -u github.com/pootwaddle/mailenable/scrubbing@latest
+go get -u github.com/pootwaddle/mailenable/scrubbing@v0.2.1
 go mod tidy
 go build
 copy *.exe \\moe\d\bjtools\ /Y
 cd ..\logparse
-go get -u github.com/pootwaddle/mailenable/scrubbing@351ee46e61bdd27008bcfea5bf060711abab44bc
+go get -u github.com/pootwaddle/mailenable/scrubbing@v0.2.1
 go mod tidy
 go build
 copy *.exe \\moe\d\bjtools\ /Y
 cd ..\malbytes
-go get -u github.com/pootwaddle/mailenable/scrubbing@351ee46e61bdd27008bcfea5bf060711abab44bc
+go get -u github.com/pootwaddle/mailenable/scrubbing@v0.2.1
 go mod tidy
 go build
 copy *.exe \\moe\d\bjtools\ /Y
 cd ..\cleanit
-go get -u github.com/pootwaddle/mailenable/scrubbing@351ee46e61bdd27008bcfea5bf060711abab44bc
+go get -u github.com/pootwaddle/mailenable/scrubbing@v0.2.1
 go mod tidy
 go build
 copy *.exe \\moe\d\bjtools\ /Y
 cd ..\greyparse
-go get -u github.com/pootwaddle/mailenable/scrubbing@351ee46e61bdd27008bcfea5bf060711abab44bc
+go get -u github.com/pootwaddle/mailenable/scrubbing@v0.2.1
 go mod tidy
 go build
 copy *.exe \\moe\d\bjtools\ /Y
 cd ..\spamparse
-go get -u github.com/pootwaddle/mailenable/scrubbing@351ee46e61bdd27008bcfea5bf060711abab44bc
+go get -u github.com/pootwaddle/mailenable/scrubbing@v0.2.1
 go mod tidy
 go build
 copy *.exe \\moe\d\bjtools\ /Y
